2eb330a60edc435b8dd8f7464704fe1a93cb2bc0
Cameron Carton <77831758+CameronCarton@users.noreply.github.com>
2023-03-22 22:25:53 +0000

    updates to list and item pages
    changed layouts of list and item pages, as well as updated main page layout

--- pages/components/ItemPage.js
+++ pages/components/ItemPage.js
@@ -13,34 +13,66 @@ const ItemPage = ({item, provider, account, dataMarket, togglePop}) => {
     }
 
     return(
-    <div class="itemPage">
+        <div class="itemPage">
         <div class="item-details">
-            <button class="close" onClick={togglePop}>x</button>
             <div class="item-details2">
-                <div class="item-overview">
-                    
-                    <div class="item-image" id="item-image">
-                        <img src={item.image} alt="Item"></img>
-                    </div>
-                    <div class="item-overview2">
+
+                <div class="item-image" id="item-image">
+                    <img src={item.image} alt="Item"></img>
+                </div> 
+
+                <div class="item-overview2">
+
+                    <div class="item-text-box2">
+                        <p>Item Name : </p>
                         {item.name}
+                    </div>
+
+                    <div class="item-text-box2">
+                        <p>Price : </p>
                         <p>{ethers.utils.formatUnits(item.price.toString(), 'ether')} ETH</p>
+                    </div>
+
+                    <div class="item-text-box2">
+                        <p>Category : </p>
                         <p>{item.category}</p>
                     </div>
-                    <div class="item-overview3">
-                        Item Information
-                        <p>{item.information}</p>
-                    </div>
+
                 </div>
-                <div class="item-order">
-                    <div class="item-order2">
-                        {ethers.utils.formatUnits(item.price.toString(), 'ether')} ETH
-                    </div>
-                    <button class="item-buy" onClick={buyItem}>Buy Now</button>
+
+            </div>
+
+            <div class="item-text-box-files">
+                
+            </div>
+
+            <div class="item-text-box-files">
+                <div class="item-text-box2-info">
+                        <p>Description : </p>
+                        <p>{item.information}</p>
                 </div>
             </div>
+
+            <div class="item-text-box-files">
+                <div class="item-text-box2-tags">
+                        <p>Tags : </p>
+                        <p>{item.tags}</p>
+                </div>
+            </div>
+
+            <div class="item-order">
+                <div class="item-order2">
+                {ethers.utils.formatUnits(item.price.toString(), 'ether')} ETH
+                </div>
+                <button class="item-buy" onClick={buyItem}>Buy Now</button>
+            </div>
+
         </div>
+
+
     </div>
+
+    
 
     );
 }
--- pages/components/ListPage.js
+++ pages/components/ListPage.js
@@ -136,83 +136,93 @@ const ListPage = ({ items, provider, account, dataMarket, togglePop2 }) => {
     return (
       <div class="itemPage">
         <div class="item-details">
-          <button class="close" onClick={togglePop2}>
-            x
-          </button>
-          <div class="item-details2">
-            <div class="item-overview">
-              <div class="item-image" id="item-image"></div>
-  
-              <div class="item-text-box-files">
-                <div class="item-text-box2-files">
-                  Image-
-                  <input type="file" id="file-selector" accept="image/jpeg" />
-                  <label htmlFor="file-selector" class="button"></label>
-                </div>
-                <div class="item-text-box2-files">
-                  Data-
-                  <input type="file" id="file-selector2" accept="text/*" />
-                  <label htmlFor="file-selector" class="button"></label>
+            <div class="item-details2">
 
-                  {dataFile ? (
-                  <div>
-                      {data.map((col,idx) => <div key={idx}>{col}</div>)}
-                  </div>
-                  ):(
-                    <p>null</p>
-                  )}
-                </div>
-              </div>
-                    
-              <div class="item-overview2">
-              <div class="item-text-box2">
-                Item Name
-                <input class="item-text-box" type="text" placeholder="" onChange={nameChange}/>
-              </div>
-              <div class="item-text-box2">
-                Price (ETH)
-                <input
-                  class="item-text-box-price"
-                  type="number"
-                  step="0.0001"
-                  min="0.0001"
-                  value={price}
-                  onChange={handlePriceChange}
-                />
-                </div>
-                <div class="item-text-box2">Category 
-                    <div class="category">
-                        <button onClick={() => toggleCategory()}>{selectedCategory}</button>
-                        {categoryToggle &&(
-                            <div class="category-content">
-                            <a href="#" onClick={() => setCategory('Category 1')}>Category 1</a>
-                            <a href="#" onClick={() => setCategory('Category 2')}>Category 2</a>
-                            <a href="#" onClick={() => setCategory('Category 3')}>Category 3</a>
-                            <a href="#" onClick={() => setCategory('Category 4')}>Category 4</a>
-                            <a href="#" onClick={() => setCategory('Category 5')}>Category 5</a>
-                            <a href="#" onClick={() => setCategory('Category 6')}>Category 6</a>
-                            <a href="#" onClick={() => setCategory('Category 7')}>Category 7</a>
-                            <a href="#" onClick={() => setCategory('Category 8')}>Category 8</a>
-                            </div>
-                        )}
+                <div class="item-image" id="item-image"></div> 
+
+                <div class="item-overview2">
+
+                    <div class="item-text-box2">Item Name
+                      <input class="item-text-box" type="text" placeholder="" onChange={nameChange}/>
                     </div>
-                </div>
-                    <div class="item-text-box2-tags">Item Tags 
-                        <textarea class="item-text-box-tags" rows="2" onChange={tagsChange}></textarea>
+
+                    <div class="item-text-box2">Price (ETH)
+                      <input
+                        class="item-text-box-price"
+                        type="number"
+                        step="0.0001"
+                        min="0.0001"
+                        value={price}
+                        onChange={handlePriceChange}
+                      />
                     </div>
-                    <div class="item-text-box2-info">Information
-                        <textarea class="item-text-box-info" rows="2" onChange={infoChange}></textarea>
+
+                    <div class="item-text-box2">Category 
+                        <div class="category">
+                            <button onClick={() => toggleCategory()}>{selectedCategory}</button>
+                            {categoryToggle &&(
+                                <div class="category-content">
+                                <a href="#" onClick={() => setCategory('Category 1')}>Category 1</a>
+                                <a href="#" onClick={() => setCategory('Category 2')}>Category 2</a>
+                                <a href="#" onClick={() => setCategory('Category 3')}>Category 3</a>
+                                <a href="#" onClick={() => setCategory('Category 4')}>Category 4</a>
+                                <a href="#" onClick={() => setCategory('Category 5')}>Category 5</a>
+                                <a href="#" onClick={() => setCategory('Category 6')}>Category 6</a>
+                                <a href="#" onClick={() => setCategory('Category 7')}>Category 7</a>
+                                <a href="#" onClick={() => setCategory('Category 8')}>Category 8</a>
+                                </div>
+                            )}
+                        </div>
                     </div>
-                </div>
+
+                    <div class="item-text-box2-files">Image-
+                      <input type="file" id="file-selector" accept="image/jpeg" />
+                      <label htmlFor="file-selector" class="button"></label>
+                    </div>
+
+                    <div class="item-text-box2-files">Data-
+                      <input type="file" id="file-selector2" accept="text/*" />
+                      <label htmlFor="file-selector" class="button"></label>
+
+                      {dataFile ? (
+                      <div>
+                          {data.map((col,idx) => <div key={idx}>{col}</div>)}
+                      </div>
+                      ):(
+                        <p>null</p>
+                      )}
+
+                    </div>
 
                 </div>
-                <div class="item-order">
-                    <div class="item-order2">
-                    </div>
-                    <button class="item-buy" onClick={() => listItem()}>List Item</button>
+
+            </div>
+
+            <div class="item-text-box-files">
+                
+            </div>
+
+            <div class="item-text-box-files">
+                <div class="item-text-box2-info">Description
+                  <textarea class="item-text-box-info" rows="2" onChange={infoChange}></textarea>
                 </div>
             </div>
+
+            <div class="item-text-box-files">
+                <div class="item-text-box2-tags">Item Tags 
+                    <textarea class="item-text-box-tags" rows="2" onChange={tagsChange}></textarea>
+                </div>
+            </div>
+
+            <div class="item-order">
+                <div class="item-order2">
+                </div>
+                <button class="item-buy" onClick={() => listItem()}>List Item</button>
+            </div>
+
         </div>
+
+
     </div>
 
     );
